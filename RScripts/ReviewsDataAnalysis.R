b1afe65069d459928643a88f883af1bfd31ec4f7
cperez <cperez@adobe.com>
2016-10-30 09:31:02 -0400

    added the creation of more figures from the Leskovec paper

--- RScripts/ReviewsDataAnalysis.R
+++ RScripts/ReviewsDataAnalysis.R
@@ -1,7 +1,39 @@
 
 library(data.table)
 library(ggplot2)
+library(sqldf)
 setwd('/Users/cperez/Desktop/iTunesData')
+
+## Todo list:
+#Polish:
+
+#For app nodes:
+#1. p_e(d)
+#2. Figure 2
+
+#For users as nodes:
+
+
+#Create:
+#1. app node lifetimes
+#2. user node lifetimes
+
+#3. number of app nodes over time
+#4. number of user nodes over time
+
+
+
+#####################
+### Quick stats
+#####################
+
+#1. number of userids:
+#542k
+nrow(as.data.frame(unique(reviewMetaData$userid)))
+#2. number of appids:
+#8.057k
+nrow(as.data.frame(unique(reviewMetaData$appid)))
+
 
 #####################
 ### 1. Read in data 
@@ -159,6 +191,10 @@ colnames(possibleTimestamps) = c("unixTimestamp")
 appMinTimes = aggregate(appDegreesPerTimestamp$unixTimestamp, by = list(appDegreesPerTimestamp$appid), FUN = min)
 setnames(appMinTimes, "Group.1", "appid")
 setnames(appMinTimes, "x", "minTime")
+appMaxTimes = aggregate(appDegreesPerTimestamp$unixTimestamp, by = list(appDegreesPerTimestamp$appid), FUN = max)
+setnames(appMaxTimes, "Group.1", "appid")
+setnames(appMaxTimes, "x", "maxTime")
+
 
 appDegreeEvolution =  merge(appDegreesPerTimestamp, appMinTimes, by = c("appid"))
 uniqueApps = as.data.frame(unique(appDegreeEvolution$appid))
@@ -179,6 +215,7 @@ possibleAppsTimes = possibleAppsTimes[order(possibleAppsTimes[c("appid", "unixTi
 
 possibleAppsTimes$degreeAtPriorTimestamp = shift(possibleAppsTimes$completeDegreeAtTimeT, -1)
 possibleAppsTimes$appDummyCol = 1
+
 numAppsWithDegreeDAtPriorTimestamp = aggregate(possibleAppsTimes$appDummyCol, by = list(possibleAppsTimes$unixTimestamp, possibleAppsTimes$degreeAtPriorTimestamp), FUN = sum)
 setnames(numAppsWithDegreeDAtPriorTimestamp, "Group.1", "unixTimestamp")
 setnames(numAppsWithDegreeDAtPriorTimestamp, "Group.2", "degreeAtPriorTimestamp")
@@ -215,6 +252,56 @@ ggsave("Figure1_Attempt.jpg")
 #ggplot() + geom_point(data = prob_PA_a[which(prob_PA_a$prob_e_d < 1 & prob_PA_a$degreeAtPriorTimestamp < 5000),], aes(x =degreeAtPriorTimestamp, y = prob_e_d )) + labs(x = "degree")
 
 
+
+
+### Instead, averaging point estimates:
+#Want to merge on timestamp and unixTimestamp:
+
+
+possibleAppsTimes$appDummyCol = 1
+numAppsWithDegreeD_t = aggregate(possibleAppsTimes$appDummyCol, by = list(possibleAppsTimes$unixTimestamp, possibleAppsTimes$degreeAtPriorTimestamp), FUN = sum)
+setnames(numAppsWithDegreeD_t, "Group.1", "unixTimestamp")
+setnames(numAppsWithDegreeD_t, "Group.2", "degreeAtPriorTimestamp")
+setnames(numAppsWithDegreeD_t, "x", "numApps")
+
+
+numEdgesWithPriorTimeDegreeD_t = aggregate(possibleAppsTimes$numNewUsersAtTimestamp, by = list(possibleAppsTimes$unixTimestamp, possibleAppsTimes$degreeAtPriorTimestamp), FUN = sum)
+setnames(numEdgesWithPriorTimeDegreeD_t, "Group.1", "unixTimestamp")
+setnames(numEdgesWithPriorTimeDegreeD_t, "Group.2", "degreeAtPriorTimestamp")
+setnames(numEdgesWithPriorTimeDegreeD_t, "x", "numEdges")
+
+totalNumEdges_t = aggregate(possibleAppsTimes$numNewUsersAtTimestamp, by = list(possibleAppsTimes$unixTimestamp), FUN = sum)
+setnames(totalNumEdges_t, "Group.1", "unixTimestamp")
+setnames(totalNumEdges_t, "x", "totalNumEdges")
+
+
+prob_PA_t = merge(totalNumEdges_t, numEdgesWithPriorTimeDegreeD_t, by = c( "unixTimestamp"))
+prob_PA_t$numEdges = as.double(prob_PA_t$numEdges)
+prob_PA_t$totalNumEdges = as.double(prob_PA_t$totalNumEdges)
+
+prob_PA_t$prob_e_d = prob_PA_t$numEdges / prob_PA_t$totalNumEdges
+
+#Adding the denom:
+possibleAppsTimes$appDummyCol = 1
+numAppsWithDegreeD = aggregate(possibleAppsTimes$appDummyCol, by = list(possibleAppsTimes$degreeAtPriorTimestamp), FUN = sum)
+setnames(numAppsWithDegreeD, "Group.1", "degreeAtPriorTimestamp")
+setnames(numAppsWithDegreeD, "x", "numApps")
+#Calculating the numerator:
+prob_PA_SummedOverTime = aggregate(prob_PA_t$prob_e_d, by = list(prob_PA_t$degreeAtPriorTimestamp), FUN = sum)
+setnames(prob_PA_SummedOverTime, "Group.1", "degreeAtPriorTimestamp")
+setnames(prob_PA_SummedOverTime, "x", "totalProbabilitySum")
+
+prob_PA_SummedOverTime = merge(prob_PA_SummedOverTime, numAppsWithDegreeD, by = c( "degreeAtPriorTimestamp"))
+prob_PA_SummedOverTime$prob_e_d = prob_PA_SummedOverTime$totalProbabilitySum / prob_PA_SummedOverTime$numApps
+
+prob_PA_SummedOverTime$log_prob_e_d = log(prob_PA_SummedOverTime$prob_e_d)
+
+ggplot() + geom_point(data = prob_PA_SummedOverTime, aes(x = log(degreeAtPriorTimestamp), y = log_prob_e_d )) + labs(x = "degree") +
+  labs(title = "Figure 1: Probability p_e(d) of a new edge e choosing a destination at a node of degree d")
+
+ggsave("Figure1_ProbabilityOfEdgeEChoosingDestinationWithDegreeD.png")
+
+
 #######
 ### B. Now, look at edges formed with other users (assuming the two-hop (user -> app -> fellow user of same app))
 #######
@@ -223,10 +310,70 @@ ggsave("Figure1_Attempt.jpg")
 
 
 #####################
-## 4. Figure 2: Average number of edges created by a node of age a.
+## 4. Figures 2 and 7: Average number of edges created by a node of age a, exponential model of node ages.
 #####################
+#Note: for our context, we should probably do this separately for reviwers and apps
+
+#A. First, do this only for apps
+#1. attach the min times per app to get the age of an app:
+appNumEdgesByAge = possibleAppsTimes[which(is.na(possibleAppsTimes$appid) == F),]
+appNumEdgesByAge = merge(appNumEdgesByAge, appMaxTimes, by = c("appid"))
+appNumEdgesByAge$appAgeInWeeks = round(((appNumEdgesByAge$unixTimestamp - appNumEdgesByAge$minTime)/(24*60*60*7)))
+appNumEdgesByAge$appLifetime = round(((appNumEdgesByAge$maxTime - appNumEdgesByAge$minTime)/(24*60*60)))
 
 
+#2. Calculate the numerator:
+averageCreatedEdgesPerWeeks = aggregate(appNumEdgesByAge$numNewUsersAtTimestamp, by = list(appNumEdgesByAge$appAgeInWeeks), FUN = sum)
+setnames(averageCreatedEdgesPerWeeks, "Group.1", "appAgeInWeeks")
+setnames(averageCreatedEdgesPerWeeks, "x", "numNewUsersAtTimestamp")
+
+#3. Calculate the denominator:
+#To get all nodes that ever achieved a specific age a, we do a cross join on (all possible ages) x (apps's max ages):
+maxAppAges = aggregate(appNumEdgesByAge$appAgeInWeeks, by = list(appNumEdgesByAge$appid), FUN = max)
+setnames(maxAppAges, "Group.1", "appid")
+setnames(maxAppAges, "x", "maxAppAge")
+
+allPossibleAges = as.data.frame(unique(averageCreatedEdgesPerWeeks$appAgeInWeeks))
+colnames(allPossibleAges) = c("possibleAge")
+
+numAppsAchievingAge = merge(allPossibleAges, maxAppAges, by = NULL)
+numAppsAchievingAge = numAppsAchievingAge[which(numAppsAchievingAge$maxAppAge >= numAppsAchievingAge$possibleAge),]
+numAppsAchievingAge$appDummyCol = 1
+numAppsAchievingAge = aggregate(numAppsAchievingAge$appDummyCol, by = list(numAppsAchievingAge$possibleAge), FUN = sum)
+setnames(numAppsAchievingAge, "Group.1", "possibleAge")
+setnames(numAppsAchievingAge, "x", "numAppsAchievingAge")
+
+#4. merge the numerators and the denoms, and then calculate the avg. number of edges:
+avgCreatedEdgesAtAgeA = merge(averageCreatedEdgesPerWeeks, numAppsAchievingAge, by.x = c("appAgeInWeeks"), by.y = c("possibleAge"))
+avgCreatedEdgesAtAgeA$avgNumEdgesCreatedAtAgeA = avgCreatedEdgesAtAgeA$numNewUsersAtTimestamp / avgCreatedEdgesAtAgeA$numAppsAchievingAge
+
+ggplot() + geom_point(data = avgCreatedEdgesAtAgeA, aes(x =appAgeInWeeks, y = avgNumEdgesCreatedAtAgeA )) + labs(x = "Node age (weeks)") + 
+  labs(title = "Figure 2: Average number of edges created by nodes at age a",  x = "Node age (weeks)")
+ggsave("AvgNumEdgesCreatedByNodesAtAgeA_all.png")
+#Without outliers:
+ggplot() + geom_point(data = avgCreatedEdgesAtAgeA[which(avgCreatedEdgesAtAgeA$appAgeInWeeks < 175),], aes(x =appAgeInWeeks, y = avgNumEdgesCreatedAtAgeA )) + labs(x = "Node age (weeks)") +
+  labs(title = "Figure 2: Average number of edges created by nodes at age a, app age < 175 weeks",  x = "Node age (weeks)")
+ggsave("AvgNumEdgesCreatedByNodesAtAgeA_lessThan175Weeks.png")
+
+ggplot() + geom_point(data = avgCreatedEdgesAtAgeA[which(avgCreatedEdgesAtAgeA$appAgeInWeeks < 150),], aes(x =appAgeInWeeks, y = avgNumEdgesCreatedAtAgeA )) + 
+  labs(title = "Figure 2: Average number of edges created by nodes at age a, app age < 150 weeks",  x = "Node age (weeks)")
+ggsave("AvgNumEdgesCreatedByNodesAtAgeA_lessThan150Weeks.png")
+
+
+
+### Fitting an exponential model to node ages:
+fit1 <- fitdistr(appNumEdgesByAge$appLifetime, "exponential") 
+lambda = .0024415
+
+appNumEdgesByAge$lifetimeExponentialFit = lambda*exp(-lambda*appNumEdgesByAge$appLifetime)
+
+avgLifetimeExponentialFits = aggregate(appNumEdgesByAge$lifetimeExponentialFit, by = list(appNumEdgesByAge$appLifetime), FUN = mean)
+setnames(avgLifetimeExponentialFits, "Group.1", "appLifetime")
+setnames(avgLifetimeExponentialFits, "x", "avgLifetimeExponentialFit")
+
+ggplot() + geom_point(data = avgLifetimeExponentialFits, aes(x =appLifetime, y = log(avgLifetimeExponentialFit) )) + 
+  labs(title = "Figure 7: Exponentially-distributed node lifetimes",  x = "Node lifetime (days)")
+ggsave("ExponentiallyDistLifetimesByDays.png")
 
 
 
@@ -235,6 +382,9 @@ ggsave("Figure1_Attempt.jpg")
 #####################
 ## 5. Figure 4: Number of edges E_h created to nodes h hops away.
 #####################
+# Hmm, the notion of hops for users-apps edges seems a bit tricky.
+# An app two-hops away requires that the user is connected via: user -> app_1 -> otherUserUsingApp_1 -> app_2
+
 
 
 
@@ -243,9 +393,15 @@ ggsave("Figure1_Attempt.jpg")
 ## 5. Figure 5: Probability of linking to a random node h hops from source node.
 #####################
 
+
+
 #####################
 ## 5. Figure 6: Node lifetimes
 #####################
+
+#
+  
+
 
 
 #####################
@@ -266,8 +422,44 @@ appDegreesPerTimestamp = as.data.frame(unique(appDegreesPerTimestamp[c("appid", 
 appDegreesPerTimestamp$numTimesTest = 1
 appDegreesPerTimestamp = aggregate(appDegreesPerTimestamp$numTimesTest, by = list(appDegreesPerTimestamp$appid), FUN = sum)
 
+#Number of unique users per app:
+userAppEarliestEdges$testCol = 1
+numUsersPerApp = aggregate(userAppEarliestEdges$testCol, by = list(userAppEarliestEdges$appid), FUN = sum)
+setnames(numUsersPerApp, "Group.1", "appid")
+setnames(numUsersPerApp, "x", "numUsers")
+
+summary(numUsersPerApp$x)
+quants_numUsersApp = quantile(numUsersPerApp$x, probs = c(.8, .9, .95, .975, .99, .995))
+
+#####################
+### 6. Creating the user-user graph using app-hops
+#####################
+
+#1. add a threshold parameter for edges between users only if both have reviewed at least n apps
+# user_a -> app_1 -> user_b, with optional parameter on # of common apps reviewed.
+
+#So, we create a (user,user) edgelist with a attribute on # of apps reviewd.
+#We do this using a self-join:
+
+#(appid, userid, (earliest) unixTimestamp)
+
+#userEdgeList = merge(userAppEarliestEdges, userAppEarliestEdges, by = c("appid"))
+
+userAppEarliestEdges = merge(userAppEarliestEdges, numUsersPerApp, by = c("appid"))
+
+#> quants_numUsersApp
+#80%    90%    95%  97.5%    99%  99.5% 
+#  14.0   42.0  120.0  360.0 1360.2 2639.4 
+userAppEarliestEdges_lo = userAppEarliestEdges[which(userAppEarliestEdges$numUsers <= 10000),]
+userAppEarliestEdges_hi = userAppEarliestEdges[which(userAppEarliestEdges$numUsers > 10000),]
 
 
+userEdgeList_lo = sqldf('select A.userid AS userid_A, B.userid AS userid_B, COUNT(*) AS numCommonApps from userAppEarliestEdges_lo AS A INNER JOIN userAppEarliestEdges_lo AS B 
+                        ON A.appid = B.appid AND A.userid > B.userid GROUP BY A.userid, B.userid')
+
+
+userEdgeList_hi = sqldf('select A.userid AS userid_A, B.userid AS userid_B, COUNT(*) AS numCommonApps from userAppEarliestEdges_hi AS A INNER JOIN userAppEarliestEdges_hi AS B 
+                        ON A.appid = B.appid AND A.userid > B.userid GROUP BY A.userid, B.userid')
 
 
 
